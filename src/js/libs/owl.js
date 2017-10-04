7bcd385564e7490c7c858a0c355eec9889ed0624
xolms <xolms111@ya.ru>
2017-10-04 15:06:19 +0300

    adaptive company

--- src/js/libs/owl.js
+++ src/js/libs/owl.js
@@ -1,26 +1,24 @@
-
-var company = $('.company__slider').owlCarousel({
+$('.company__slider').owlCarousel({
     nav: false,
     loop: true,
     dots: false,
+    responsiveClass:true,
     responsive:{
         0:{
             items:1
         },
-        500: {
-            items: 2
+        800:{
+            items:2
         },
-        1000:{
+        1100:{
             items:3
-        },
-        1200:{
-            items:4
         },
         1600: {
             items:5
         }
     }
 });
+var company = $('.company__slider').owlCarousel();
 $('.company__nav_right').click(function () {
     company.trigger('next.owl.carousel');
 });

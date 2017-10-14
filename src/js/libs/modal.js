113592f5b65014b7df54c871c3ba0121743bb661
xolms <xolms111@ya.ru>
2017-10-14 20:06:34 +0300

    123

--- src/js/libs/modal.js
+++ src/js/libs/modal.js
@@ -5,10 +5,14 @@ $('.modal-open').click(function (e) {
     e.preventDefault();
     var modal = $(this).attr('data-modal');
     var theme = $(this).attr('data-theme');
-    if (modal == '.modal-presentation') {
-        var title = $(this).attr('data-title');
-        var color = $(this).attr('data-color');
+    var title = $(this).attr('data-title');
+    if(title) {
         $(modal+' .modal__title').text(title);
+    }
+    if (modal == '.modal-presentation') {
+
+        var color = $(this).attr('data-color');
+
         $(modal+' .modal__title').removeClass().addClass('modal__title').addClass('modal__title_'+color);
         $(modal+' .modal__button').removeClass().addClass('modal__button').addClass('modal__button_'+color);
     }
